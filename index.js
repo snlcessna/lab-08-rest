1dd3248a461e589512ee75d7fd5f76ecfd0c922b
David Boucher <david.boucher@ookla.com>
2017-10-14 11:05:33 -0700

    Server Setup

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict';
 
 require("dotenv").config();
-require("./lib/server").start();
+
+require('./lib/server.js').start(process.argv[2] || process.env.PORT || 3000)
+    .then(serverReply => console.log(serverReply))
+    .catch(serverError => console.log('Server error: ', serverError));
