538c34e39cdf46c1a9a72e4c46e8c7f9d9ed93c8
Larry Cessna <larry.cessna@outlook.com>
2017-10-14 14:40:23 -0700

    Server.js and router.js updates. Still not working.

--- note/route.js
+++ note/route.js
@@ -1,43 +1,46 @@
 'use strict';
 
-const Note = require('../model/note.js');
-const router = require('../lib/router.js');
+const Note = require('../note/router.js');
+const router = require('../lib/router.js').routeHandlers;
 
 let notes = {};
 
-router.post('/api/notes', (req,res) => {
-    // 400 when?
-    // Save the note to the stack
-    // Send 200
-
-});
-
-router.delete('/api/notes', (req,res) => {
-    // Do I have an id?
-    // Is it valid
-    // Nuke it
-    // Send 200 if all is well
-
-});
-
-router.put('/api/notes', (req,res) => {
-    // Do I have an id?
-    // Is it valid
-    // Replace it
-    // Send 200 if all is well
-
-});
-
-router.patch('/api/notes', (req,res) => {
-    // Do I have an id?
-    // Is it valid
-    // Update it
-    // Send 200 if all is well
-
-});
+// router.post('/api/notes', (req,res) => {
+//
+//     res.writeHead(404, {'Content-Type': 'text/plain'});
+//
+//     // 400 when?
+//     // Save the note to the stack
+//     // Send 200
+//
+// });
+//
+// router.delete('/api/notes', (req,res) => {
+//     // Do I have an id?
+//     // Is it valid
+//     // Nuke it
+//     // Send 200 if all is well
+//
+// });
+//
+// router.put('/api/notes', (req,res) => {
+//     // Do I have an id?
+//     // Is it valid
+//     // Replace it
+//     // Send 200 if all is well
+//
+// });
+//
+// router.patch('/api/notes', (req,res) => {
+//     // Do I have an id?
+//     // Is it valid
+//     // Update it
+//     // Send 200 if all is well
+//
+// });
 
 router.get('/api/notes', (req,res) => {
-
+  
     // If we have an id
         // try and pull it from the stack
             // send it
