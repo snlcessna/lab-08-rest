20f1ec2a98c57f7d9b6a7b9b08d15a7536ad938c
David Boucher <david.boucher@ookla.com>
2017-10-17 13:23:14 -0700

    clean up code and add sendMessage function for responses

--- note/route.js
+++ note/route.js
@@ -3,24 +3,16 @@
 const Note = require('../note/model.js');
 const router = require('../lib/router.js');
 const parser = require('../lib/parse-request');
+const sendMessage = require('../lib/sendMessage.js');
 let notes = {};
 
 router.POST('/api/notes', (req,res) => {
 //pass data as stringifed JSON in the body of a POST request to create a new resource
     if(!req.body) {
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.write('No content found.');
-        res.end();
+        sendMessage(res, 400, 'No content found.');
     } else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(`Found content: ${req.body}`);
-        new Note;
-        res.end();
+        sendMessage(res, 200, `Found content: ${req.body}`);
     }
-    // 400 when?
-    // Save the note to the stack
-    // Send 200
-
 });
 //
 router.DELETE('/api/notes', (req,res) => {
@@ -29,59 +21,40 @@ router.DELETE('/api/notes', (req,res) => {
 
     if (Object.keys(req.url.query).length > 0) {
         if (typeof notes[req.url.query['uuid']] === 'undefined') {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('Note not found. Please try another uuid');
-            res.end();
+            sendMessage(res, 404, 'Note not found. Please try another uuid');
+
         } else {
             notes[req.url.query['uuid']] = null;
             delete notes[req.url.query['uuid']];
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.write('Note Deleted');
-            res.end();
+            sendMessage(res, 200, 'Note Deleted.');
         }
     } else {
         // nothing passed as ?parameter
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Please send a ?uuid= parameter with your POST request');
-        res.end();
+        sendMessage(res, 404, 'Please send a ?uuid= parameter with your POST request');
     }
-
-
 });
 
 router.GET('/api/notes', (req,res) => {
-  parser(req);
+    parser(req);
     if (Object.keys(req.url.query).length > 0) {
-      if(typeof notes[req.url.query['uuid']] === 'undefined'){
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('Note not found')
-        res.end();
-    } else {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(notes[req.url.query['uuid']]);
-      res.end();
-    }
-  } else {
-      //if no query exists, send all responses
-      if(Object.keys(notes).length > 0){
-        for (var note in notes) {
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.write(notes.note);
+        if(typeof notes[req.url.query['uuid']] === 'undefined'){
+            sendMessage(res, 404, 'Note not found');
+        } else {
+            sendMessage(res, 200, notes[req.url.query['uuid']]);
         }
-      }else{
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.write('You do not have any notes');
+    } else {
+        //if no query exists, send all responses
+        if(Object.keys(notes).length > 0){
+            for (var note in notes) {
+                //sendMessage() does not apply here
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.write(notes.note);
+            }
+        }else{
+            sendMessage(res, 400, 'You do not have any notes');
+        }
         res.end();
-      }
-      res.end();
-  }
-    // If we have an id
-    // try and pull it from the stack
-    // send it
-    // 404 not found or 400 invalid query?
-
-    // List all
-
+    }
 });
 
 module.exports = router;
